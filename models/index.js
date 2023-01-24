bee94fcb1f20dec75067422ae42b97b7d3cf406a
Dexter Dick <@dexterdick@hotmail.com>
2023-01-24 06:36:36 -0500

    fixed error in models index

--- models/index.js
+++ models/index.js
@@ -3,25 +3,25 @@ const Blog = require('./Blog');
 const Comment = require('./Comment');
 
 User.belongsToMany(Blog, {
-  through:{
+  through: {
     model: Comment,
-    unique: false
-  }
+    unique: false,
+  },
 });
 
 Blog.belongsToMany(User, {
   through: {
     model: Comment,
-    unique: false
-  } 
+    unique: false,
+  },
 });
 
 User.hasMany(Blog, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
 });
 
-Blog.belonngsTo(User, {
+Blog.belongsTo(User, {
   foreignKey: 'user_id',
-})
+});
 
 module.exports = { User, Blog, Comment };
