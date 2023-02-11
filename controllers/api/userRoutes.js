aad911fad9c99824bd8850cdf6d4505196c1a5da
Dexter Dick <@dexterdick@hotmail.com>
2023-02-10 22:37:49 -0500

    made update to userRoute to login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { response } = require('express');
 const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
@@ -10,6 +11,42 @@ router.post('/', async (req, res) => {
       req.session.logged_in = true;
 
       res.status(200).json(userData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: { username: req.body.userName },
+    });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.json({
+        user: userData,
+        message: 'You are logged in!',
+      });
     });
   } catch (err) {
     console.log(err);
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -25,7 +25,30 @@ router.get('/', async (req, res) => {
   }
 });
 
+// withAuth middleware prevents acesss to route if user is not loggin
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // check if user is logged in bassed on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Blog }],
+    });
+
+    const user = userData.get({ plain: true });
+    res.render('dashboard', {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
   res.render('login');
 });
 
